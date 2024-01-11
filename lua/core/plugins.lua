716802fc3802a102ab9245603202bd780f9dd191
Daniel Hauenstein <daniel@slimsec.de>
2024-01-11 08:48:52 +0100

    added noice

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -78,6 +78,21 @@ local plugins = {
     event = {"CmdlineEnter"},
     ft = {"go", 'gomod'},
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
   }
 
 
