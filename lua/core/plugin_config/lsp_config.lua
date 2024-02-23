cc15be40a6ded1941eadedcb34d24507c275a9b2
Daniel Hauenstein <dh@slimsec.de>
2024-02-23 08:34:16 +0000

    added auto install for mason

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,6 +1,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "html"}
+  ensure_installed = { "lua_ls", "html"},
+  automatic_installation = true
 })
 
 local on_attach = function(_,_) 
